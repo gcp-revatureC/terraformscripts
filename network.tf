ddf2d9de59dff55bcead6b5bbf5a35abd46a00d2
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-08-26 16:41:07 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -7,17 +7,18 @@ Provider "google" {
 resource "google_compute_network" "vpc_network" {
 name      = "vpc-network" 
 region = "us-central1"
+gateway_ipv4 = "10.2.0.0/16"
 
 auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "subnet1" {
     name = "sub1"
-    ip_cidr_range = "10.2.0.0/16"
+    ip_cidr_range = "10.2.0.0/24"
     region = "us-cental1"
     network = "google_compute_network.vpc_network.self_test" 
     secondary_ip_range {
         range_name = "subnet2"
-        ipc_cidr_range = "10.2.0.0/24" 
+        ipc_cidr_range = "10.2.0.0/32" 
     }
 }
 resource "google_compute_vpn_gateway" "gateway1" { 
