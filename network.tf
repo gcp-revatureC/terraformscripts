77b49fa0bfadb9e813e9fa3d24e6f1eafb77f1ca
ashishsaxena7 <49167200+ashishsaxena7@users.noreply.github.com>
2019-08-29 14:30:37 -0400

    expounded upon networks

--- network.tf
+++ network.tf
@@ -1,13 +1,43 @@
-
 resource "google_compute_network" "vpc_network" {
-  name         = "vpc-network"
+  name         = "storage-network"
+  ipv4_range = 10.1.0.0/16
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "subnet1" {
+resource "google_compute_network" "vpc_network2" {
+  name         = "application-network"
+    ipv4_range = 10.2.0.0/16
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet1-1" {
   name          = "sub1"
-  ip_cidr_range = "10.2.0.0/24"
+  ip_cidr_range = "10.1.0.0/24"
   network       = "${google_compute_network.vpc_network.self_link}"
+}
+
+resource "google_compute_subnetwork" "subnet2-1" {
+  name          = "sub2"
+  ip_cidr_range = "10.1.1.0/24"
+  network       = "${google_compute_network.vpc_network.self_link}"
+}
+
+resource "google_compute_subnetwork" "subnet1-2" {
+  name          = "sub1"
+  ip_cidr_range = "10.2.0.255/24"
+  network       = "${google_compute_network.vpc_network2.self_link}"
+}
+
+resource "google_compute_subnetwork" "subnet2-2" {
+  name          = "sub2"
+  ip_cidr_range = "10.2.0.0/25"
+  network       = "${google_compute_network.vpc_network2.self_link}"
+}
+
+resource "google_compute_subnetwork" "subnet3-2" {
+  name          = "sub3"
+  ip_cidr_range = "10.2.0.128/25"
+  network       = "${google_compute_network.vpc_network2.self_link}"
 }
 
 resource "google_compute_vpn_gateway" "gateway1" {
